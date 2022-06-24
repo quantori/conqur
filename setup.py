253067475173c11706f36cef9f6fb4b6a012fc00
a.grefenshteyn <aleksandrgrefenstejn@gmail.com>
2022-06-24 16:53:56 +0300

    Corrections of comments.

--- setup.py
+++ setup.py
@@ -9,19 +9,17 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 requirements = [
-    "pip==19.2.3",
-    "bump2version==0.5.11",
-    "wheel==0.33.6",
-    "watchdog==0.9.0",
-    "flake8==3.7.8",
-    "tox==3.14.0",
-    "numpy",
-    "pandas",
-    "scikit-learn",
-    "setuptools",
+    "numpy>=1.21.6",
+    "scikit-learn>=1.0.2",
+    "setuptools>=60.2.0"
 ]
 
-test_requirements = []
+extras = {
+    "test": [
+        "pandas>=1.3.0",
+    ]
+}
+
 
 setup(
     author="Aleksandr Grefenstein",
@@ -37,21 +35,20 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    description="Batch effects removal for microbiome data via conditional quantile",
+    description="Batch effects removal for microbiome data via conditional quantile regression",
     entry_points={
         "console_scripts": [
             "conqur=conqur.cli:main",
         ],
     },
     install_requires=requirements,
+    extras_require=extras,
     license="MIT license",
     long_description=readme + "\n\n" + history,
     include_package_data=True,
     keywords="conqur",
     name="conqur",
     packages=find_packages(include=["conqur", "conqur.*"]),
-    test_suite="tests",
-    tests_require=test_requirements,
     url="https://github.com/Karkarmath/conqur",
     version="0.1.0",
     zip_safe=False,
